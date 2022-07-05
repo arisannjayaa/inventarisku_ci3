c70f72cc6e6ee9275c7905c3fd9ac5a5465c0622
arisannjayaa <wayanarisanjaya01@gmail.com>
2022-07-05 19:07:12 +0800

    jurusan ❤️❤️❤️❤️❤️

--- application/views/jurusan/tambah.php
+++ application/views/jurusan/tambah.php
@@ -1,26 +1,15 @@
 <div class="card">
 	<div class="card-content">
 		<div class="card-body">
-			<form class="form form-horizontal" method="post" action="#">
+			<form class="form form-horizontal" method="post" action="<?= base_url('jurusan/add_prosess') ?>">
 				<div class="form-body">
 					<div class="row">
 						<div class="col-md-4">
-							<label>Nama Barang</label>
+							<label>Nama Jurusan</label>
 						</div>
 						<div class="col-md-8 form-group">
 							<input type="text" class="form-control" name="nama" placeholder="Nama Barang">
-						</div>
-						<div class="col-md-4">
-							<label>Stok Barang</label>
-						</div>
-						<div class="col-md-8 form-group">
-							<input type="number" class="form-control" name="stok" placeholder="Stok Barang">
-						</div>
-						<div class="col-md-4">
-							<label>Harga Barang</label>
-						</div>
-						<div class="col-md-8 form-group">
-							<input type="number" class="form-control" name="harga" placeholder="Harga Baranh">
+							<small class="text-danger"><?= form_error('nama') ?></small>
 						</div>
 						<div class="col-sm-12 d-flex justify-content-end">
 							<button type="submit" class="btn btn-primary me-1 mb-1">Simpan</button>
