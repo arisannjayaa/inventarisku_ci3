62e62b58ad23c1eb1dc5fa46075e340a270e906f
arisannjayaa <wayanarisanjaya01@gmail.com>
2022-07-10 23:49:45 +0800

    change to stable template

--- database/db_inventarisku_ci3.sql
+++ database/db_inventarisku_ci3.sql
@@ -2,48 +2,6 @@ create database db_inventarisku_ci3;
 
 use db_inventarisku_ci3;
 
-create table tb_user(
-	id_user int primary key auto_increment,
-	nim varchar(20),
-	nama_lengkap varchar(100),
-	id_jurusan char(2),
-	id_prodi char(4),
-	no_telp varchar(20),
-	email varchar(100),
-	agama varchar(50),
-	alamat varchar(255),
-	jenis_kelamin varchar(20),
-	username varchar(30),
-	password varchar(255),
-	tanggal_lahir date,
-	avatar varchar(50),
-	foreign key (id_jurusan) references tb_jurusan(id_jurusan),
-	foreign key (id_prodi) references tb_prodi(id_prodi)
-	level enum('admin', 'mahasiswa');
-);
-
-insert into tb_user(username, password) values
-('admin', sha1('admin'), 'admin'),
-('siswa1', sha1('admin'), 'mahasiswa'),
-('siswa2', sha1('admin'), 'mahasiswa'),
-
-create table tb_barang(
-	id_barang int primary key auto_increment,
-	nama_barang varchar(50),
-	stok_barang int,
-	harga_barang int,
-	gambar_barang varchar(255)
-);
-
-insert into tb_barang(nama_barang, stok_barang, harga_barang) values 
-('Mixer Yamaha MGP10XU', 1, 50000),
-('Mix Krezt Beta58', 1, 35000),
-('Mic Wireless Ashley MC Pro 1', 1, 45000),
-('Stand Mic', 5, 12000),
-('Green Screen Fulset', 1, 20000),
-('Tripod Kamera', 2, 20000),
-('Zhiyun Smooth 2', 1, 70000);
-
 create table tb_jurusan(
 	id_jurusan char(2) primary key,
 	nama_jurusan varchar(100)
@@ -74,6 +32,49 @@ insert into tb_prodi(id_prodi, id_jurusan, nama_prodi) values
 ('AK02', 'AK', 'Akuntansi Manajerial'),
 ('AN01', 'AN', 'Administrasi Bisnis'),
 ('AN02', 'AN', 'Manajemen Bisnis Internasional');
+
+create table tb_user(
+	id_user int primary key auto_increment,
+	nim varchar(20),
+	nama_lengkap varchar(100),
+	id_jurusan char(2),
+	id_prodi char(4),
+	no_telp varchar(20),
+	email varchar(100),
+	agama varchar(50),
+	alamat varchar(255),
+	jenis_kelamin varchar(20),
+	username varchar(30),
+	password varchar(255),
+	tanggal_lahir date,
+	avatar varchar(50),
+	foreign key (id_jurusan) references tb_jurusan(id_jurusan),
+	foreign key (id_prodi) references tb_prodi(id_prodi),
+	level enum('admin', 'mahasiswa')
+);
+
+insert into tb_user(username, password, level) values
+('admin', sha1('admin'), 'admin'),
+('siswa1', sha1('admin'), 'mahasiswa'),
+('siswa2', sha1('admin'), 'mahasiswa');
+
+create table tb_barang(
+	id_barang int primary key auto_increment,
+	nama_barang varchar(50),
+	stok_barang int,
+	harga_barang int,
+	gambar_barang varchar(255)
+);
+
+insert into tb_barang(nama_barang, stok_barang, harga_barang) values 
+('Mixer Yamaha MGP10XU', 1, 50000),
+('Mix Krezt Beta58', 1, 35000),
+('Mic Wireless Ashley MC Pro 1', 1, 45000),
+('Stand Mic', 5, 12000),
+('Green Screen Fulset', 1, 20000),
+('Tripod Kamera', 2, 20000),
+('Zhiyun Smooth 2', 1, 70000);
+
 
 create table tb_orders(
 	id_orders int primary key auto_increment,
